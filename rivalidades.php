5a3593dcd2ded2d0c6acefb2efeed98471c050fb
Sexernal <salazarjocksan2005@gmail.com>
2025-03-21 20:45:54 -0600

    rivalidadesFIN

--- rivalidades.php
+++ rivalidades.php
@@ -5,61 +5,111 @@
     <div class="rivalidades-container">
         <h2>Las Rivalidades</h2>
 
-        <!-- Tarjeta de Alajuelense -->
+        <!-- Tarjeta de liGAY -->
         <div id="alajuelense" class="tarjeta-rivalidad">
             <img src="imagen/rivalidades/lda2.png" alt="Alajuelense" class="logo-ligay">
-            <h3>Alajuelense</h3>
-            <p>
-                El Superclásico del fútbol costarricense es el partido de fútbol disputado por los clubes Deportivo Saprissa
-                y Liga Deportiva Alajuelense de la Primera División de Costa Rica. Según encuestas los seguidores de estos
-                equipos constituyen cerca del 75% de hinchas al fútbol del país, el 70% de la población aproximadamente.
-                Saprissa es el equipo tibaseño, de la Provincia de San José, equipo de la capital porque se fundó en
-                San José en 1935 y Alajuelense es el equipo de Alajuela, la segunda ciudad más poblada del país.
-            </p>
-            <video controls>
-                <source src="videos/rivalidades/alajuelense.mp4" type="video/mp4">
-                Tu navegador no soporta el elemento de video.
-            </video>
-            <a href="https://es.wikipedia.org/wiki/Cl%C3%A1sico_del_f%C3%BAtbol_costarricense" class="boton" target="_blank">
-                Más información
-            </a>
+
+            <!-- Contenedor general -->
+            <div class="contenido">
+                <!-- Contenedor para el texto -->
+                <div class="texto-rivalidad">
+                    <h3>Alajuelense</h3>
+                    <p>
+                        El Superclásico del fútbol costarricense es el partido de fútbol disputado por los clubes Deportivo Saprissa
+                        y Liga Deportiva Alajuelense de la Primera División de Costa Rica. Según encuestas los seguidores de estos
+                        equipos constituyen cerca del 75% de hinchas al fútbol del país, el 70% de la población aproximadamente.
+                        Saprissa es el equipo tibaseño, de la Provincia de San José, equipo de la capital porque se fundó en
+                        San José en 1935 y Alajuelense es el equipo de Alajuela, la segunda ciudad más poblada del país.
+                    </p>
+                    <a href="https://es.wikipedia.org/wiki/Cl%C3%A1sico_del_f%C3%BAtbol_costarricense"
+                        class="boton" target="_blank">
+                        Más información
+                    </a>
+                </div>
+
+                <!-- Contenedor para el video -->
+                <div class="video-rivalidad">
+                    <iframe
+                        width="560"
+                        height="315"
+                        src="https://www.youtube.com/embed/raA3vqEevq0"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                </div>
+            </div>
         </div>
 
-        <!-- Tarjeta de Herediano -->
+        <!-- Tarjeta de _erediANO -->
         <div id="herediano" class="tarjeta-rivalidad">
             <img src="imagen/rivalidades/here2.png" alt="Herediano" class="logo-perredia">
-            <h3>Herediano</h3>
-            <p>
-                Los enfrentamientos entre el Saprissa y el Herediano comenzaron oficialmente en 1949 por el campeonato
-                nacional de la primera división del fútbol costarricense, con un balance que ha sido favorable a los
-                morados en múltiples capítulos escritos en los estadios Nacional, Eladio Rosabal, Ricardo Saprissa,
-                "Colleya" Fonseca y Carlos Alvarado.
-            </p>
-            <video controls>
-                <source src="videos/rivalidades/herediano.mp4" type="video/mp4">
-                Tu navegador no soporta el elemento de video.
-            </video>
-            <a href="https://es.wikipedia.org/wiki/Cl%C3%A1sico_del_Buen_F%C3%BAtbol_Costa_Rica" class="boton" target="_blank">
-                Más información
-            </a>
+
+            <!-- Contenedor general -->
+            <div class="contenido">
+                <!-- Contenedor para el texto -->
+                <div class="texto-rivalidad">
+                    <h3>Herediano</h3>
+                    <p>
+                        Los enfrentamientos entre el Saprissa y el Herediano comenzaron oficialmente en 1949 por el campeonato
+                        nacional de la primera división del fútbol costarricense, con un balance que ha sido favorable a los
+                        morados en múltiples capítulos escritos en los estadios Nacional, Eladio Rosabal, Ricardo Saprissa,
+                        "Colleya" Fonseca y Carlos Alvarado.
+                    </p>
+                    <a href="https://es.wikipedia.org/wiki/Cl%C3%A1sico_del_Buen_F%C3%BAtbol_Costa_Rica"
+                        class="boton" target="_blank">
+                        Más información
+                    </a>
+                </div>
+
+                <!-- Contenedor para el video -->
+                <div class="video-rivalidad">
+                    <iframe
+                        width="560"
+                        height="315"
+                        src="https://www.youtube.com/embed/Cc_nBdZYDXI"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                </div>
+            </div>
         </div>
 
-        <!-- Tarjeta de Cartaginés -->
+
+        <!-- Tarjeta de cartucho -->
         <div id="cartagines" class="tarjeta-rivalidad">
             <img src="imagen/rivalidades/cartago.png" alt="Cartaginés" class="logo-cartucho">
-            <h3>Cartaginés</h3>
-            <p>
-                Clásico de las metrópolis es el partido que disputan el Deportivo Saprissa y el Club Sport Cartaginés,
-                llamado así debido a que tanto San José como Cartago han sido capitales del país.
-            </p>
-            <video controls>
-                <source src="videos/rivalidades/cartagines.mp4" type="video/mp4">
-                Tu navegador no soporta el elemento de video.
-            </video>
-            <a href="https://es.wikipedia.org/wiki/Anexo:Cl%C3%A1sicos_del_f%C3%BAtbol_costarricense" class="boton" target="_blank">
-                Más información
-            </a>
+
+            <!-- Contenedor general -->
+            <div class="contenido">
+                <!-- Contenedor para el texto -->
+                <div class="texto-rivalidad">
+                    <h3>Cartaginés</h3>
+                    <p>
+                        Clásico de las metrópolis es el partido que disputan el Deportivo Saprissa y el Club Sport Cartaginés,
+                        llamado así debido a que tanto San José como Cartago han sido capitales del país.
+                    </p>
+                    <a href="https://es.wikipedia.org/wiki/Anexo:Cl%C3%A1sicos_del_f%C3%BAtbol_costarricense"
+                        class="boton" target="_blank">
+                        Más información
+                    </a>
+                </div>
+
+                <!-- Contenedor para el video -->
+                <div class="video-rivalidad">
+                    <iframe
+                        width="560"
+                        height="315"
+                        src="https://www.youtube.com/embed/FJ8RjqevW_c?"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                </div>
+            </div>
         </div>
+
         <!-- Aquí puedes agregar más equipos de rivalidad -->
     </div>
 </section>
