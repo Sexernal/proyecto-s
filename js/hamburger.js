5b1bcb31bc811781a84b2ecacc28e24ca0dc495e
Sexernal <salazarjocksan2005@gmail.com>
2025-03-29 18:42:50 -0600

    revision1

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,10 +1,7 @@
 // Mostrar/ocultar menú en móviles
 const hamburger = document.getElementById("hamburger");
 const navbarMenu = document.getElementById("navbar-menu");
-const contenidoAdicional = document.querySelector(".contenido-adicional");
 
 hamburger.addEventListener("click", () => {
-    console.log("Botón de hamburguesa clickeado"); // Verifica en la consola
     navbarMenu.classList.toggle("active");
-    contenidoAdicional.classList.toggle("active");
 });
--- parte_superior.php
+++ parte_superior.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/jpg" href="imagen/mounstro.png">
     <title>Moradictos</title>
+    <!-- Hojas de estilo principales y modulares -->
     <link rel="stylesheet" href="./css/main.css">
     <link rel="stylesheet" href="./css/modulos/menu.css">
     <link rel="stylesheet" href="./css/modulos/inicio.css">
@@ -24,7 +25,6 @@
 </head>
 
 <body>
-
     <!-- Menú de navegación -->
     <nav class="navbar">
         <div class="navbar-container">
@@ -44,31 +44,29 @@
             <ul class="navbar-menu" id="navbar-menu">
                 <li><a href="index.php">Inicio</a></li>
                 <li class="dropdown">
-                    <a href="historia.php">Historia del Saprissa</a>
+                    <a href="historia.php">Historia</a>
                     <ul class="submenu">
                         <li><a href="#fundacion">Fundación</a></li>
                         <li><a href="#titulos">Títulos</a></li>
                         <li><a href="#jugadores-historicos">Jugadores Históricos</a></li>
                     </ul>
                 </li>
-                <li><a href="noticias.php">Noticias de la S</a></li>
-                <li><a href="galeria.php">Fotos y Videos</a></li>
-                <li class="dropdown">
-                    <a href="plantilla.php">Plantilla Actual</a>
-                </li>
+                <li><a href="noticias.php">Noticias</a></li>
+                <li><a href="galeria.php">Fotos Videos</a></li>
+                <li><a href="plantilla.php">Plantilla</a></li>
                 <li><a href="ultra-morada.php">Ultra Morada</a></li>
                 <li class="dropdown">
-                    <a href="rivalidades.php">Las Rivalidades</a>
+                    <a href="rivalidades.php">Rivalidades</a>
                     <ul class="submenu">
                         <li><a href="#alajuelense">Alajuelense</a></li>
                         <li><a href="#herediano">Herediano</a></li>
                         <li><a href="#cartagines">Cartaginés</a></li>
                     </ul>
                 </li>
-                <li><a href="estadio.php">Estadio Ricardo Saprissa</a></li>
-                <li><a href="canticos.php">Cánticos y Porras</a></li>
+                <li><a href="estadio.php">Estadio</a></li>
+                <li><a href="canticos.php">Cánticos</a></li>
                 <li><a href="contacto.php">Contacto</a></li>
-                <!-- Nuevo elemento "Otros" con submenú -->
+                <!-- Submenú "Otros" -->
                 <li class="dropdown">
                     <a href="#otros">Otros</a>
                     <ul class="submenu">
